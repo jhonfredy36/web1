e26a796d048f9b6d0f898d787174ee39dbf50f15
jhonfredy36 <127421007+jhonfredy36@users.noreply.github.com>
2023-05-29 19:31:39 -0500

    punto2 +identacion

--- script.js
+++ script.js
@@ -18,56 +18,65 @@ while (menu < 7) {
     if (menu==1) {
 
         contadorUno+=1
-    // ejercicio 1
-let num1,num2,num3;
-num1= parseInt(prompt("ingrese el primer numero"))
-num2= parseInt(prompt("ingrese el segundo numero"))
-num3= parseInt(prompt("ingrese el tercer numero"))
+        // ejercicio 1
+        let num1,num2,num3;
+        num1= parseInt(prompt("ingrese el primer numero"))
+        num2= parseInt(prompt("ingrese el segundo numero"))
+        num3= parseInt(prompt("ingrese el tercer numero"))
 
-if (num1<num2 && num1<num3) {
-    menor=num1
-    if(num2<num3){
-        console.log("el orden ascendente es es" +num1,+num2,+num3)
+        if (num1<num2 && num1<num3) {
+            menor=num1
+            if(num2<num3){
+                console.log("el orden ascendente es es" +num1,+num2,+num3)
 
-    }else
-    console.log("el orden ascendente es es" +num1,+num3,+num2)
+            }else
+                console.log("el orden ascendente es es" +num1,+num3,+num2)
 
-}else if(num2<num1 && num2<num3){
-    
-    if (num1<num3) {
-        console.log("el orden ascendente es es" +num2,+num1,+num3)
-    }else{
-        console.log("el orden ascendente es es" +num2,+num3,+num1)
-    }
-}else if (num3<num1 && num3<num2) {
-    if(num1<num2){
-        console.log("el orden ascendente es es" +num3,+num1,+num2)
-    }else{
-        console.log("el orden ascendente es es" +num3,+num2,+num1)
-    }  
-}
+            }else if(num2<num1 && num2<num3){
 
-if (num1>num2 && num1>num3) {
-    menor=num1
-    if(num2>num3){
-        console.log("el orden descendente es es" +num1,+num2,+num3)
+                if (num1<num3) {
+                    console.log("el orden ascendente es es" +num2,+num1,+num3)
+                }else{
+                    console.log("el orden ascendente es es" +num2,+num3,+num1)
+                }
+            }else if (num3<num1 && num3<num2) {
+                if(num1<num2){
+                    console.log("el orden ascendente es es" +num3,+num1,+num2)
+                }else{
+                    console.log("el orden ascendente es es" +num3,+num2,+num1)
+                }  
+            }
 
-    }else
-    console.log("el orden descendente es es" +num1,+num3,+num2)
+            if (num1>num2 && num1>num3) {
+                menor=num1
+                if(num2>num3){
+                    console.log("el orden descendente es es" +num1,+num2,+num3)
+                }else
+                    console.log("el orden descendente es es" +num1,+num3,+num2)
+            }else if(num2>num1 && num2>num3){
+                if (num1>num3) {
+                    console.log("el orden descendente es es" +num2,+num1,+num3)
+                }else{
+                    console.log("el orden descendente es es" +num2,+num3,+num1)
+                }
+            }else if (num3>num1 && num3>num2) {
+                if(num1>num2){
+                    console.log("el orden descendente es es" +num3,+num1,+num2)
+                }else{
+                    console.log("el orden descendente es es" +num3,+num2,+num1)
+                }  
+            }
+        menuInicio()
+    }else if (menu==2) {
 
-}else if(num2>num1 && num2>num3){
-    if (num1>num3) {
-        console.log("el orden descendente es es" +num2,+num1,+num3)
-    }else{
-        console.log("el orden descendente es es" +num2,+num3,+num1)
-    }
-}else if (num3>num1 && num3>num2) {
-    if(num1>num2){
-        console.log("el orden descendente es es" +num3,+num1,+num2)
-    }else{
-        console.log("el orden descendente es es" +num3,+num2,+num1)
-    }  
-}
-menuInicio()
+        contadorDos+=1
+        let diametro,radio,pi,area
+
+        diametro=parseInt(prompt("ingrese el diametro del circulo"))
+        radio=diametro*0.5
+        pi=3.14
+        area=pi*(radio*radio) 
+        console.log("el area del circulo es:"+area)
+        menuInicio()
     }
 }
